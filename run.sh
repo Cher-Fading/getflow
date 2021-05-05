243d783c000dd518d574f3953fc4fcf5b1d44dbd
Cher-Fading <xw31@illinois.edu>
2021-05-05 16:39:52 -0500

    commit

--- run.sh
+++ run.sh
@@ -12,13 +12,17 @@ cd ~/Flow
 #indexline=$1
 linenumber=0
 while IFS= read -r line; do
-	if [ $1 -eq $linenumber ]; then
-		mkdir -p '/usatlas/scratch/cher97/tempin'$linenumber
-		xrdcp 'root://dcgftp.usatlas.bnl.gov:1096/'$line '/usatlas/scratch/cher97/tempin'$linenumber		
-		filename=/usatlas/scratch/cher97/tempin$linenumber/$(ls /usatlas/scratch/cher97/tempin$linenumber)
-		
+	if [ $5 -eq $linenumber ]; then
+		mkdir -p '/usatlas/scratch/cher97/tempin'$3_$linenumber
+		xrdcp 'root://dcgftp.usatlas.bnl.gov:1096/'$line '/usatlas/scratch/cher97/tempin'$3_$linenumber		
+		filename=$(ls /usatlas/scratch/cher97/tempin$3_$linenumber)
+		cp /usatlas/u/cher97/flow/sources/MyAnalysis/share/ATestRun_eljob.py /usatlas/scratch/cher97/tempin$3_$linenumber/
+		chmod +X /usatlas/scratch/cher97/tempin$3_$linenumber/ATestRun_eljob.py
+		sed -i "s/^inputFilePath = .*/inputFilePath = '/usatlas/scratch/cher97/tempin$3_$linenumber'/" /usatlas/scratch/cher97/tempin$3_$linenumber/ATestRun_eljob.py
+    	sed -i "s/^ROOT.SH.ScanDir().filePattern(.*/ROOT.SH.ScanDir().filePattern( '$filename').scan( sh, inputFilePath )/" /usatlas/scratch/cher97/tempin$3_$linenumber/ATestRun_eljob.py
+		cat /usatlas/scratch/cher97/tempin$3_$linenumber/ATestRun_eljob.py
 		sleep 2
-		rm -rf '/usatlas/scratch/cher97/tempin'$linenumber
+		rm -rf '/usatlas/scratch/cher97/tempin'$3_$linenumber
 	fi
 	linenumber=$((linenumber + 1))
 done <"$input"
