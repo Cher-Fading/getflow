0ab0eaa1bf3628a6c54f113d6da5b95ab8678f10
Xiaoning Wang <xw31@illinois.edu>
2021-05-05 19:14:44 -0400

    commit

--- run.sh
+++ run.sh
@@ -5,22 +5,28 @@
 input="../getflow/$3_$4_root$2.txt"
 #input="mc16_5TeV_short.txt"
 
-mkdir -p /usatlas/scratch/cher97/$1$2_$3_$4_$5
+mkdir -p /usatlas/scratch/cher97/$1$2_$3_$4
 
 #indexline=$1
 linenumber=0
 while IFS= read -r line; do
 	if [ $5 -eq $linenumber ]; then
-		mkdir -p '/usatlas/scratch/cher97/tempin'$3_$linenumber
-		#xrdcp 'root://dcgftp.usatlas.bnl.gov:1096/'$line '/usatlas/scratch/cher97/tempin'$3_$linenumber		
-		filename=$(ls /usatlas/scratch/cher97/tempin$3_$linenumber)
-		cp /usatlas/u/cher97/flow/sources/MyAnalysis/share/ATestRun_eljob.py /usatlas/scratch/cher97/tempin$3_$linenumber/
-		chmod +X /usatlas/scratch/cher97/tempin$3_$linenumber/ATestRun_eljob.py
-		sed -i "s@^inputFilePath = .*@inputFilePath = '/usatlas/scratch/cher97/tempin$3_$linenumber'@" /usatlas/scratch/cher97/tempin$3_$linenumber/ATestRun_eljob.py
-    	sed -i "s/^ROOT.SH.ScanDir().filePattern(.*/ROOT.SH.ScanDir().filePattern( '$filename').scan( sh, inputFilePath )/" /usatlas/scratch/cher97/tempin$3_$linenumber/ATestRun_eljob.py
-		cat /usatlas/scratch/cher97/tempin$3_$linenumber/ATestRun_eljob.py
+		mkdir -p '/usatlas/scratch/cher97/tempin'$3_$4_$linenumber
+		xrdcp 'root://dcgftp.usatlas.bnl.gov:1096/'$line '/usatlas/scratch/cher97/tempin'$3_$4_$linenumber
+		cd /usatlas/scratch/cher97/tempin$3_$4_$linenumber/
+		filename=$(ls *.root*)
+#		echo $filename
+		cp /usatlas/u/cher97/flow/source/MyAnalysis/share/ATestRun_eljob.py /usatlas/scratch/cher97/tempin$3_$4_$linenumber/
+		chmod +x /usatlas/scratch/cher97/tempin$3_$4_$linenumber/ATestRun_eljob.py
+		sed -i "s@^inputFilePath = .*@inputFilePath = '/usatlas/scratch/cher97/tempin$3_$4_$linenumber'@" /usatlas/scratch/cher97/tempin$3_$4_$linenumber/ATestRun_eljob.py
+    		sed -i "s@^ROOT.SH.ScanDir().filePattern(.*@ROOT.SH.ScanDir().filePattern( '$filename').scan( sh, inputFilePath )@" /usatlas/scratch/cher97/tempin$3_$4_$linenumber/ATestRun_eljob.py
+		cd /usatlas/scratch/cher97/tempin$3_$4_$linenumber/
+		rm -rf submitDir
+		./ATestRun_eljob.py --submission-dir=submitDir
+		cp submitDir/data-myOutput/tempin$3_$4_$linenumber.root /usatlas/scratch/cher97/$1$2_$3_$4/tempin$3_$4_$linenumber.root
+#		cat /usatlas/scratch/cher97/tempin$3_$4_$linenumber/ATestRun_eljob.py
 		sleep 2
-		#rm -rf '/usatlas/scratch/cher97/tempin'$3_$linenumber
+		#rm -rf '/usatlas/scratch/cher97/tempin'$3_$4_linenumber
 	fi
 	linenumber=$((linenumber + 1))
 done <"$input"
