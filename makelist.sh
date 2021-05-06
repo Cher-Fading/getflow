6e083b01dc3b9e5f42c9b95e933b3ac566ec25bb
Xiaoning Wang <xw31@illinois.edu>
2021-05-06 03:04:28 -0400

    commit

---
+++ makelist.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rucio list-dids data18_hi.00*.calibration_PCPEB.merge.AOD.k1029_m2048 > dataset.txt
+
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd ~/flow
 asetup --restore
 cd build
@@ -7,13 +8,13 @@ input="~/getflow/$1_runlist.txt"
 
 linenumber=0
 while IFS= read -r line; do
-    cp ~/getflow/run_temp.job ~/getflow/run_PC$line.job
-    sed -i "s@^Arguments.*@Arguments       = $1 _pnfs $line PC \$(Process)'@" ~/getflow/run_PC$line.job
-    condor_submit run_PC$line.job
+	cp ~/getflow/run_temp.job ~/getflow/run_PC$line.job
+	sed -i "s@^Arguments.*@Arguments       = $1 _pnfs $line PC \$(Process)'@" ~/getflow/run_PC$line.job
+	condor_submit run_PC$line.job
     
-    cp ~/getflow/run_temp.job ~/getflow/run_CC$line.job
-    sed -i "s@^Arguments.*@Arguments       = $1 _pnfs $line CC \$(Process)'@" ~/getflow/run_CC$line.job
-    condor_submit run_CC$line.job
-    
+#	cp ~/getflow/run_temp.job ~/getflow/run_CC$line.job
+#	sed -i "s@^Arguments.*@Arguments       = $1 _pnfs $line CC \$(Process)'@" ~/getflow/run_CC$line.job
+#	condor_submit run_CC$line.job
+
 	linenumber=$((linenumber + 1))
 done <"$input"
