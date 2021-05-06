2c5d45dc94c00fe9b568167610070862d212bf13
Xiaoning Wang <xw31@illinois.edu>
2021-05-06 04:44:01 -0400

    commit

--- runloop.sh
+++ runloop.sh
@@ -2,7 +2,7 @@
 #input="/atlasgpfs01/usatlas/data/cher97/mc16_5TeV.txt"
 # ./run.sh Flow210504.1 _pnfs 365678 PC 3
 
-input="~/getflow/$3_$4_root$2.txt"
+input=~/getflow/$3_$4_root$2.txt
 #input="mc16_5TeV_short.txt"
 
 mkdir -p /usatlas/scratch/cher97/$1$2_$3_$4
@@ -26,8 +26,8 @@ while IFS= read -r line; do
 		cp submitDir/data-myOutput/tempin$3_$4_$linenumber.root /usatlas/scratch/cher97/$1$2_$3_$4/tempin$3_$4_$linenumber.root
 #		cat /usatlas/scratch/cher97/tempin$3_$4_$linenumber/ATestRun_eljob.py
 		sleep 2
-		#rm -rf '/usatlas/scratch/cher97/tempin'$3_$4_linenumber
+		rm -rf '/usatlas/scratch/cher97/tempin'$3_$4_linenumber
 	fi
 	linenumber=$((linenumber + 1))
-done <"$input"
+done <$input
 
