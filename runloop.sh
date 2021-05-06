4c64e6f68f62566246fe5fbf26727fa0395134e6
Cher-Fading <xw31@illinois.edu>
2021-05-06 01:46:52 -0500

    commit

--- runloop.sh
+++ runloop.sh
@@ -2,7 +2,7 @@
 #input="/atlasgpfs01/usatlas/data/cher97/mc16_5TeV.txt"
 # ./run.sh Flow210504.1 _pnfs 365678 PC 3
 
-input="../getflow/$3_$4_root$2.txt"
+input="~/getflow/$3_$4_root$2.txt"
 #input="mc16_5TeV_short.txt"
 
 mkdir -p /usatlas/scratch/cher97/$1$2_$3_$4
@@ -19,7 +19,7 @@ while IFS= read -r line; do
 		cp /usatlas/u/cher97/flow/source/MyAnalysis/share/ATestRun_eljob.py /usatlas/scratch/cher97/tempin$3_$4_$linenumber/
 		chmod +x /usatlas/scratch/cher97/tempin$3_$4_$linenumber/ATestRun_eljob.py
 		sed -i "s@^inputFilePath = .*@inputFilePath = '/usatlas/scratch/cher97/tempin$3_$4_$linenumber'@" /usatlas/scratch/cher97/tempin$3_$4_$linenumber/ATestRun_eljob.py
-    		sed -i "s@^ROOT.SH.ScanDir().filePattern(.*@ROOT.SH.ScanDir().filePattern( '$filename').scan( sh, inputFilePath )@" /usatlas/scratch/cher97/tempin$3_$4_$linenumber/ATestRun_eljob.py
+    	sed -i "s@^ROOT.SH.ScanDir().filePattern(.*@ROOT.SH.ScanDir().filePattern( '$filename').scan( sh, inputFilePath )@" /usatlas/scratch/cher97/tempin$3_$4_$linenumber/ATestRun_eljob.py
 		cd /usatlas/scratch/cher97/tempin$3_$4_$linenumber/
 		rm -rf submitDir
 		./ATestRun_eljob.py --submission-dir=submitDir
