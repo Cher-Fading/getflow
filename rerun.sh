0f98108d062ddef10d45e393bbbad9522d246807
Xiaoning Wang <xw31@illinois.edu>
2021-05-11 01:04:23 -0400

    commit

--- rerun.sh
+++ rerun.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-cd /usatlas/scratch/cher97/
-ls $1_pnfs_$2_$3 >~/getflow/$1_pnfs_$2_$3_stat.txt
+#./rerun.sh Flow210505.1 365678 CC May
+cd /pnfs/usatlas.bnl.gov/users/cher97/
+ls -h -l $1_pnfs_$2_$3 >~/getflow/$1_pnfs_$2_$3_stat.txt
 
 input=~/getflow/$1_pnfs_$2_$3_stat.txt
 #cat ~/getflow/$1_runlist.txt
 
+rm ~/getflow/$1_pnfs_$2_$3_rerun.txt
+touch ~/getflow/$1_pnfs_$2_$3_rerun.txt
+
 linenumber=0
 while IFS= read -r line; do
 	b=${line#*'usatlas '}
+#	echo $b
 	c=${b%%' '$4*}
-	echo $c
-	if [ "$c" == "0" ]; then
-		$linenumber >>~/getflow/$1_pnfs_$2_$3_rerun.txt
+	d=${b##*'_'}
+	e=${d%%'.root'*}
+#	echo $c
+	if [ $c == 0 ]; then
+		echo $e
+		echo $e >>~/getflow/$1_pnfs_$2_$3_rerun.txt
+		linenumber=$((linenumber + 1))
 	fi
-	linenumber=$((linenumber + 1))
+	
 done <$input
 
 cp ~/getflow/run_$3$2.job ~/getflow/rerun_$3$2.job
-sed -i "s@^Queue.*@Queue $linenumber'@" ~/getflow/rerun_$3$2.job
-sed -i "s@^Executable.*@Executable   = /usatlas/u/cher97/getflow/rerunloop.sh'@" ~/getflow/rerun_$3$2.job
-condor_submit rerun_$3$2.job
+sed -i "s@^Queue.*@Queue $linenumber@" ~/getflow/rerun_$3$2.job
+sed -i "s@^Executable.*@Executable   = /usatlas/u/cher97/getflow/rerunloop.sh@" ~/getflow/rerun_$3$2.job
+condor_submit ~/getflow/rerun_$3$2.job
