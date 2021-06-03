a0d5b886bea8f261678e32ebd1a42d5d72161627
Cher-Fading <xw31@illinois.edu>
2021-06-03 04:37:03 -0500

    commit

--- runcalqdiffsubloop.sh
+++ runcalqdiffsubloop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ./runcalqloop.sh Flow210504.1 _pnfs 365678 PC 43 Calq210510.1 2
-mkdir -p /usatlas/scratch/cher97/$1$2_$3_$4_calq_$6_v$7
+#mkdir -p /usatlas/scratch/cher97/$1$2_$3_$4_calq_$6_v$7
 input=~/getflow/$1$2_$3_$4_rootlist.txt
 #cat ~/getflow/$1$2_$3_$4_rootlist.txt
 
@@ -10,20 +10,23 @@ input=~/getflow/$1$2_$3_$4_rootlist.txt
 linenumber=-2
 while IFS= read -r line; do
 	if [ $5 -eq $linenumber ]; then
-		tempdir=`mktemp -d`
-		cd $tempdir
-		echo $tempdir
-		mkdir $tempdir/'tempin'$3_$4$linenumber
-		xrdcp $line $tempdir/'tempin'$3_$4$linenumber		
-		cd $tempdir/'tempin'$3_$4$linenumber
-		filename=$(ls *.root*)
-		mkdir $tempdir/'tempout'$3_$4$linenumber
-		cd ~/calq
-		root -b -q -l 'calq_diff_subsample.cpp("'$tempdir/tempin$3_$4$linenumber/$filename'","'$tempdir/'tempout'$3_$4$linenumber'",'$7','$8','$9')'
-		xrdcp $tempdir/tempout$3_$4$linenumber/*.root root://dcgftp.usatlas.bnl.gov:1096//pnfs/usatlas.bnl.gov/users/cher97/$1$2_$3_$4_calq_$6_v$7/${filename%%.*}_calq_$8_$9.root
-		echo ${filename%%.*}
-		sleep 2
-		rm -rf $tempdir
+		for ((i = 1; i <= $8; i++)); do
+			tempdir=$(mktemp -d)
+			cd $tempdir
+			echo $tempdir
+			mkdir $tempdir/'tempin'$3_$4$linenumber
+			xrdcp $line $tempdir/'tempin'$3_$4$linenumber
+			cd $tempdir/'tempin'$3_$4$linenumber
+			filename=$(ls *.root*)
+			mkdir $tempdir/'tempout'$3_$4$linenumber
+			cd ~/calq
+			root -b -q -l 'calq_diff_subsample.C("'$tempdir/tempin$3_$4$linenumber/$filename'","'$tempdir/'tempout'$3_$4$linenumber'",'$7','$8','$i')'
+			#xrdcp $tempdir/tempout$3_$4$linenumber/*.root root://dcgftp.usatlas.bnl.gov:1096//pnfs/usatlas.bnl.gov/users/cher97/$1$2_$3_$4_calq_sub$6_v$7/${filename%%.*}_calq_$8_$i.root
+			cp $tempdir/tempout$3_$4$linenumber/*.root /atlasgpfs01/usatlas/data/cher97/$1$2_$3_$4_calq_subdiff$6_v$7/${filename%%.*}_calq_$8_$i.root
+			echo ${filename%%.*}_calq_$8_$i
+			sleep 2
+			rm -rf $tempdir
+		done
 	fi
 	linenumber=$((linenumber + 1))
 done <$input
--- runcalqsub.sh
+++ runcalqsub.sh
@@ -8,42 +8,42 @@ while IFS= read -r line; do
 	b=${line#*data18_hi.00}
 	c=${b%%.*}
 	d=${line#*AOD.}
-		cd ~/getflow
-rm ~/getflow/$1_pnfs_$c'_PC_rootlist.txt'
-		python pnfs_ls.py -l -o ~/getflow/$1_pnfs_$c'_PC_rootlist.txt' /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_PC/tempin'$c'_PC_*.root'
-		chmod +777 /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_PC'
-		cp ~/getflow/run_temp.job ~/getflow/runcalqsubdiff_PC$c.job
-		sed -i "s@^Executable.*@Executable   = /usatlas/u/cher97/getflow/runcalqsubloop.sh@" ~/getflow/runcalqsubdiff_PC$c.job
-		sed -i "s@^Arguments.*@Arguments       = $1 _pnfs $c PC \$(Process) $2 $3 $4@" ~/getflow/runcalqsubdiff_PC$c.job
-		nof=$(($(wc -l <~/getflow/$1_pnfs_$c'_PC_rootlist.txt') - 2))
-		sed -i "s@^Queue.*@Queue $nof@" ~/getflow/runcalqsubdiff_PC$c$i.job
-		#cat runcalq_PC$c.job
-		#rm -rf /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_PC_calq_subdiff'$2_v$3
-		#mkdir /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_PC_calq_subdiff'$2_v$3
-		#chmod +777 /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_PC_calq_subdiff'$2_v$3
-		rm -rf /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_PC_calq_subdiff'$2_v$3
-		mkdir /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_PC_calq_subdiff'$2_v$3
-		chmod +777 /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_PC_calq_subdiff'$2_v$3
-		condor_submit runcalqsubdiff_PC$c.job
+	cd ~/getflow
+	rm ~/getflow/$1_pnfs_$c'_PC_rootlist.txt'
+	python pnfs_ls.py -l -o ~/getflow/$1_pnfs_$c'_PC_rootlist.txt' /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_PC/tempin'$c'_PC_*.root'
+	chmod +777 /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_PC'
+	cp ~/getflow/run_temp.job ~/getflow/runcalqsub_PC$c.job
+	sed -i "s@^Executable.*@Executable   = /usatlas/u/cher97/getflow/runcalqsubloop.sh@" ~/getflow/runcalqsub_PC$c.job
+	sed -i "s@^Arguments.*@Arguments       = $1 _pnfs $c PC \$(Process) $2 $3 $4@" ~/getflow/runcalqsub_PC$c.job
+	nof=$(($(wc -l <~/getflow/$1_pnfs_$c'_PC_rootlist.txt') - 2))
+	sed -i "s@^Queue.*@Queue $nof@" ~/getflow/runcalqsub_PC$c$i.job
+	#cat runcalq_PC$c.job
+	#rm -rf /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_PC_calq_sub'$2_v$3
+	#mkdir /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_PC_calq_sub'$2_v$3
+	#chmod +777 /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_PC_calq_sub'$2_v$3
+	rm -rf /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_PC_calq_sub'$2_v$3
+	mkdir /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_PC_calq_sub'$2_v$3
+	chmod +777 /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_PC_calq_sub'$2_v$3
+	condor_submit runcalqsub_PC$c.job
 
-		cd ~/getflow
-rm ~/getflow/$1_pnfs_$c'_CC_rootlist.txt'
-		python pnfs_ls.py -l -o ~/getflow/$1_pnfs_$c'_CC_rootlist.txt' /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_CC/tempin'$c'_CC_*.root'
-		chmod +777 /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_CC'
-		cp ~/getflow/run_temp.job ~/getflow/runcalqsubdiff_CC$c.job
-		sed -i "s@^Executable.*@Executable   = /usatlas/u/cher97/getflow/runcalqsubloop.sh@" ~/getflow/runcalqsubdiff_CC$c.job
-		sed -i "s@^Arguments.*@Arguments       = $1 _pnfs $c CC \$(Process) $2 $3 $4@" ~/getflow/runcalqsubdiff_CC$c.job
-		nof=$(($(wc -l <~/getflow/$1_pnfs_$c'_CC_rootlist.txt') - 2))
-		sed -i "s@^Queue.*@Queue $nof@" ~/getflow/runcalqsubdiff_CC$c.job
-		#cat runcalq_CC$c.job
+	cd ~/getflow
+	rm ~/getflow/$1_pnfs_$c'_CC_rootlist.txt'
+	python pnfs_ls.py -l -o ~/getflow/$1_pnfs_$c'_CC_rootlist.txt' /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_CC/tempin'$c'_CC_*.root'
+	chmod +777 /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_CC'
+	cp ~/getflow/run_temp.job ~/getflow/runcalqsub_CC$c.job
+	sed -i "s@^Executable.*@Executable   = /usatlas/u/cher97/getflow/runcalqsubloop.sh@" ~/getflow/runcalqsub_CC$c.job
+	sed -i "s@^Arguments.*@Arguments       = $1 _pnfs $c CC \$(Process) $2 $3 $4@" ~/getflow/runcalqsub_CC$c.job
+	nof=$(($(wc -l <~/getflow/$1_pnfs_$c'_CC_rootlist.txt') - 2))
+	sed -i "s@^Queue.*@Queue $nof@" ~/getflow/runcalqsub_CC$c.job
+	#cat runcalq_CC$c.job
 
-		#rm -rf /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_CC_calq_subdiff'$2_v$3
-		#mkdir /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_CC_calq_subdiff'$2_v$3
-		#chmod +777 /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_CC_calq_subdiff'$2_v$3
-		rm -rf /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_CC_calq_subdiff'$2_v$3
-		mkdir /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_CC_calq_subdiff'$2_v$3
-		chmod +777 /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_CC_calq_subdiff'$2_v$3
-		condor_submit runcalqsubdiff_CC$c.job
+	#rm -rf /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_CC_calq_sub'$2_v$3
+	#mkdir /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_CC_calq_sub'$2_v$3
+	#chmod +777 /pnfs/usatlas.bnl.gov/users/cher97/$1_pnfs_$c'_CC_calq_sub'$2_v$3
+	rm -rf /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_CC_calq_sub'$2_v$3
+	mkdir /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_CC_calq_sub'$2_v$3
+	chmod +777 /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_CC_calq_sub'$2_v$3
+	condor_submit runcalqsub_CC$c.job
 
 	linenumber=$((linenumber + 1))
 done <$input
