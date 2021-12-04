7b77b2a551b6fe6a9c229c88440a323ee1953735
Cher-Fading <xw31@illinois.edu>
2021-12-03 18:36:02 -0600

    commit

--- runmc.sh
+++ runmc.sh
@@ -2,7 +2,7 @@
 
 input=~/getflow/txts/$1_runlist.txt
 #cat ~/getflow/$1_runlist.txt
-# ./runsp.sh Qmean210916.1 True 0.5
+# ./runmc.sh MCE211203.1 1.0 0.3
 
 linenumber=0
 while IFS= read -r line; do
@@ -10,15 +10,16 @@ while IFS= read -r line; do
 	c=${b%%.*} #run number
 	d=${line##*.} #production tag
 	~/getflow/GetPHYSHI_single.sh $line $c $d
+	co=$c'_'$d
 
-	cp ~/getflow/condors/run_temp.job ~/getflow/condors/runmc_$c_$1.job
-	sed -i "s@^Arguments.*@Arguments       = $1 _pnfs $c PC \$(Process) $2 $3@" ~/getflow/condors/runmc$c_$1.job
-	nof=$(wc -l <~/getflow/$c\_PC_root_pnfs.txt)
+	cp ~/getflow/condors/run_temp.job ~/getflow/condors/runmc_$co_$2_$3.job
+	sed -i "s@^Arguments.*@Arguments       = $1 _pnfs $co \$(Process) $2 $3@" ~/getflow/condors/runmc$co_$2_$3.job
+	nof=$(wc -l <~/getflow/$co\_root_pnfs.txt)
 	sed -i "s@^Queue.*@Queue $nof@" ~/getflow/condors/runmc$c_$1.job
 	#cat run_PC$c.job
-	rm -rf /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_PC_qmean_'$2_$3/
-	mkdir /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$c'_PC_qmean_'$2_$3/
-	cat ~/getflow/condors/runmc$c_$1.job
+	rm -rf /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$co'_MCEff_'$2_$3/
+	mkdir /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$co'_MCEff_'$2_$3/
+	cat ~/getflow/condors/runmc$co_$2_$3.job
 	#condor_submit ~/getflow/condors/runmc$c_$1.job
 
 	linenumber=$((linenumber + 1))
