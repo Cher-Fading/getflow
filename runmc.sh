04f2392a4ac678a03772509db81ce08fc1fb6d95
Cher-Fading <xw31@illinois.edu>
2022-02-13 02:23:44 -0600

    commit

--- runmc.sh
+++ runmc.sh
@@ -4,7 +4,7 @@ input=~/getflow/txts/$1_runlist.txt
 #cat $input
 #cat ~/getflow/$1_runlist.txt
 # ./runmc.sh no1:MCE211203.1 no2:1.5(etarange) no3:0.3(min prob) no4:HITight no5:False(do eff) no6:mc no7:0.5(optional minpT default to 0.0) no8:(optional, if efficiency range is different from etamatchrange) no9:50(optional skipping first xx files)
-#./runmc.sh MCE211203.1 1.5 0.3 HITight False mc 0.0 2.5 
+#./runmc.sh MCE211203.1 1.5 0.3 HITight False mc 0.0 2.5
 #echo $1
 linenumber=0
 bold=$(tput bold)
@@ -13,11 +13,11 @@ normal=$(tput sgr0)
 
 while IFS= read -r line <&3; do
 	echo 'line_'${bold}$linenumber${normal}'-------------------------------------------------------------'
-	echo 'line_'$linenumber'-------------------------------------------------------------'>>~/getflow/txts/$1_log.txt
+	echo 'line_'$linenumber'-------------------------------------------------------------' >>~/getflow/txts/$1_log.txt
 
-if [ "$line" = "" ]; then
-break;
-fi
+	if [ "$line" = "" ]; then
+		break
+	fi
 	b=${line#*$6}
 	c=${b%%.*}    #data campaign
 	d=${line##*.} #production tag
@@ -44,12 +44,12 @@ fi
 		fi
 	fi
 	if [ "$6" = "data" ]; then
-isMC=False
+		isMC=False
 		temp=${b#*.}
 		runNum=${temp%%.*}
 		runNum=$(expr $runNum + 0)
-#echo $c
-c=$c'_'$runNum
+		#echo $c
+		c=$c'_'$runNum
 	fi
 	echo ${bold}Dataset:${normal}' '$line
 	echo ${bold}Specs:${normal}-------------------------------------------------------------
@@ -60,14 +60,14 @@ c=$c'_'$runNum
 	echo ${bold}eta eff range:${normal}' '$eta
 	echo ${bold}pT cut:${normal}' '$ptCut
 
-	echo 'Dataset: '$line>>~/getflow/txts/$1_log.txt
-	echo Specs:------------------------------------------------------------->>~/getflow/txts/$1_log.txt
+	echo 'Dataset: '$line >>~/getflow/txts/$1_log.txt
+	echo Specs:------------------------------------------------------------- >>~/getflow/txts/$1_log.txt
 	#~/getflow/GetPHYSHI_single.sh $line $c $d
 	#echo $c
-	echo 'run number: '$runNum>>~/getflow/txts/$1_log.txt
-	echo 'eta match range: '$2>>~/getflow/txts/$1_log.txt
-	echo 'eta eff range: '$eta>>~/getflow/txts/$1_log.txt
-	echo 'pT cut: '$ptCut>>~/getflow/txts/$1_log.txt
+	echo 'run number: '$runNum >>~/getflow/txts/$1_log.txt
+	echo 'eta match range: '$2 >>~/getflow/txts/$1_log.txt
+	echo 'eta eff range: '$eta >>~/getflow/txts/$1_log.txt
+	echo 'pT cut: '$ptCut >>~/getflow/txts/$1_log.txt
 
 	co=$c'_'$d
 	#echo $co
@@ -80,25 +80,25 @@ c=$c'_'$runNum
 	#nof=50
 	sed -i "s@^Queue.*@Queue $nof@" ~/getflow/condors/runmc_$co_$1_$2_$3_$4_$5.job
 	#cat run_PC$c.job
-echo '-------------------------------------------------------------------'
-echo '-------------------------------------------------------------------'>>~/getflow/txts/$1_log.txt
+	echo '-------------------------------------------------------------------'
+	echo '-------------------------------------------------------------------' >>~/getflow/txts/$1_log.txt
 
 	#exec 0<&1
 
-		read -p "Do you wish to remove old folder and rerun? (y/n) " answer
-		if [[ $answer =~ ^[Yy]$ ]]; then
-			echo "deleting original run folder"
-			rm -rf /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$co'_MCEff_'$2_$3_$4_$5/
-		else
-			echo "writing to old folder"
-		fi
+	read -p "Do you wish to remove old folder and rerun? (y/n) " answer
+	if [[ $answer =~ ^[Yy]$ ]]; then
+		echo "deleting original run folder"
+		rm -rf /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$co'_MCEff_'$2_$3_$4_$5/
+	else
+		echo "writing to old folder"
+	fi
 
 	mkdir -p /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$co'_MCEff_'$2_$3_$4_$5/
-echo $1_pnfs_$co'_MCEff_'$2_$3_$4_$5
-	echo $1_pnfs_$co'_MCEff_'$2_$3_$4_$5>>~/getflow/txts/$1_log.txt
+	echo $1_pnfs_$co'_MCEff_'$2_$3_$4_$5
+	echo $1_pnfs_$co'_MCEff_'$2_$3_$4_$5 >>~/getflow/txts/$1_log.txt
 	#cat ~/getflow/condors/runmc_$co_$1_$2_$3_$4_$5.job
 	condor_submit ~/getflow/condors/runmc_$co_$1_$2_$3_$4_$5.job
 	linenumber=$((linenumber + 1))
-	echo 'line_-------------------------------------------------------------'>>~/getflow/txts/$1_log.txt
-echo 'line_'${bold}$linenumber${normal}'-------------------------------------------------------------'
+	echo 'line_-------------------------------------------------------------' >>~/getflow/txts/$1_log.txt
+	echo 'line_'${bold}$linenumber${normal}'-------------------------------------------------------------'
 done 3<$input
