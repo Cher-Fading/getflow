ffe48e36ecf4227d8c9213b84b5da554ebabc2e7
Cher-Fading <xw31@illinois.edu>
2022-03-28 22:58:41 -0500

    commit

--- runmc.sh
+++ runmc.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-input=~/getflow/txts/$1_runlist.txt
+input=~/getflow/txts/$6_runlist.txt
 #cat $input
 #cat ~/getflow/$1_runlist.txt
 #./runmc.sh no1:MCE211203.1 no2:1.5(etarange) no3:0.3(min prob) no4:HITight no5:False(do eff) no6:mc no7:0.5(min match pT, default to 0.5) no8:(optional eta mult reco range, default 2.5) no9:1.0 (optional, multptcut, default to 1.0) no10:(optional eta mult truth range, default 2.5) no11:1.0 (optional, truthpt mult, default to 1.0) no12: 0 (optional minimum primary vertices, default to 0) no13:(optional, centrality choice, default to fcal (1)) no14:50(optional skipping first xx files)
-#./runmc.sh MCE220310.3_jet 1.5 0.3 HITight True data 
+#./runmc.sh MCE220310.3_jet 1.5 0.3 HITight True data
 #echo $1
 
 linenumber=0
 bold=$(tput bold)
 normal=$(tput sgr0)
->~/getflow/txts/$1_log.txt
+>~/getflow/txts/$1_$6_log.txt
 
 while IFS= read -r line <&3; do
 	echo 'line_'${bold}$linenumber${normal}'-------------------------------------------------------------'
-	echo 'line_'$linenumber'-------------------------------------------------------------' >>~/getflow/txts/$1_log.txt
+	echo 'line_'$linenumber'-------------------------------------------------------------' >>~/getflow/txts/$1_$6_log.txt
 
 	if [ "$line" = "" ]; then
 		break
@@ -40,13 +40,13 @@ while IFS= read -r line <&3; do
 		ptMult=$9
 	fi
 
-if [ "${10}" = "" ]; then
+	if [ "${10}" = "" ]; then
 		etaTruthMult=2.5
 	else
 		etaTruthMult=${10}
 	fi
 
-if [ "${11}" = "" ]; then
+	if [ "${11}" = "" ]; then
 		ptTruthMult=1.0
 	else
 		ptTruthMult=${11}
@@ -63,7 +63,7 @@ if [ "${11}" = "" ]; then
 		cent=${13}
 	fi
 
-	if [ "${6}" = "mc" ]; then
+	if [ "${6}" = "*mc*" ]; then
 		isMC=True
 		if [ "$c" = "15_5TeV" ]; then
 			runNum=226000
@@ -72,7 +72,7 @@ if [ "${11}" = "" ]; then
 			runNum=313000
 		fi
 	fi
-	if [ "$6" = "data" ]; then
+	if [ "$6" = "*data*" ]; then
 		isMC=False
 		temp=${b#*.}
 		runNum=${temp%%.*}
@@ -90,55 +90,55 @@ if [ "${11}" = "" ]; then
 	echo ${bold}eta multiplicity count range:${normal}' '$etaMult
 	echo ${bold}pT multiplicity count range:${normal}' '$ptMult
 	echo ${bold}pT Truth multiplicity count range:${normal}' '$ptTruthMult
-	echo ${bold}eta Truth multiplicity count range:${normal}' '$etaTruthMult	
-        echo ${bold}minimum primary:${normal}' '$primlim
+	echo ${bold}eta Truth multiplicity count range:${normal}' '$etaTruthMult
+	echo ${bold}minimum primary:${normal}' '$primlim
 	echo ${bold}Centrality Selection:${normal}' '$cent
 
-	echo 'Dataset: '$line >>~/getflow/txts/$1_log.txt
-	echo Specs:------------------------------------------------------------- >>~/getflow/txts/$1_log.txt
+	echo 'Dataset: '$line >>~/getflow/txts/$1_$6_log.txt
+	echo Specs:------------------------------------------------------------- >>~/getflow/txts/$1_$6_log.txt
 	#~/getflow/GetPHYSHI_single.sh $line $c $d
 	#echo $c
-	echo 'run number: '$runNum >>~/getflow/txts/$1_log.txt
-	echo 'eta match range: '$2 >>~/getflow/txts/$1_log.txt
-	echo 'pt match min: '$ptCut >>~/getflow/txts/$1_log.txt
-	echo 'eta multiplicity count range '$etaMult >>~/getflow/txts/$1_log.txt
-	echo 'pT multiplicity count range: '$ptMult >>~/getflow/txts/$1_log.txt
-	echo 'pT Truth multiplicity count range: '$ptTruthMult >>~/getflow/txts/$1_log.txt
-	echo 'eta Truth multiplicity count range: '$etaTruthMult >>~/getflow/txts/$1_log.txt
-	echo 'minimum primary: '$primlim >>~/getflow/txts/$1_log.txt
-	echo 'Centrality Selection: '$cent>>~/getflow/txts/$1_log.txt
+	echo 'run number: '$runNum >>~/getflow/txts/$1_$6_log.txt
+	echo 'eta match range: '$2 >>~/getflow/txts/$1_$6_log.txt
+	echo 'pt match min: '$ptCut >>~/getflow/txts/$1_$6_log.txt
+	echo 'eta multiplicity count range '$etaMult >>~/getflow/txts/$1_$6_log.txt
+	echo 'pT multiplicity count range: '$ptMult >>~/getflow/txts/$1_$6_log.txt
+	echo 'pT Truth multiplicity count range: '$ptTruthMult >>~/getflow/txts/$1_$6_log.txt
+	echo 'eta Truth multiplicity count range: '$etaTruthMult >>~/getflow/txts/$1_$6_log.txt
+	echo 'minimum primary: '$primlim >>~/getflow/txts/$1_$6_log.txt
+	echo 'Centrality Selection: '$cent >>~/getflow/txts/$1_$6_log.txt
 	co=$c'_'$d
 	#echo $co
 
-	cp ~/getflow/condors/run_temp.job ~/getflow/condors/runmc_$co_$1_$2_$3_$4_$5.job
+	cp ~/getflow/condors/run_temp.job ~/getflow/condors/runmc_$co_$1_$6_$2_$3_$4_$5.job
 
-	sed -i "s@^Executable.*@Executable   = /usatlas/u/cher97/getflow/runmcloop.sh@" ~/getflow/condors/runmc_$co_$1_$2_$3_$4_$5.job
-	sed -i "s@^Arguments.*@Arguments       = $1 _pnfs $co \$(Process) $2 $3 $4 $5 $runNum $isMC $ptCut $ptMult $etaMult $ptTruthMult $etaTruthMult $primlim $cent ${14}@" ~/getflow/condors/runmc_$co_$1_$2_$3_$4_$5.job
+	sed -i "s@^Executable.*@Executable   = /usatlas/u/cher97/getflow/runmcloop.sh@" ~/getflow/condors/runmc_$co_$1_$6_$2_$3_$4_$5.job
+	sed -i "s@^Arguments.*@Arguments       = $1_$6 _pnfs $co \$(Process) $2 $3 $4 $5 $runNum $isMC $ptCut $ptMult $etaMult $ptTruthMult $etaTruthMult $primlim $cent ${14}@" ~/getflow/condors/runmc_$co_$1_$6_$2_$3_$4_$5.job
 	nofful=$(wc -l <~/getflow/txts/$co\_root_pnfs.txt)
-nof=$nofful
+	nof=$nofful
 	#nof=$((nofful/4))
 	nof=$nofful
-	sed -i "s@^Queue.*@Queue $nof@" ~/getflow/condors/runmc_$co_$1_$2_$3_$4_$5.job
+	sed -i "s@^Queue.*@Queue $nof@" ~/getflow/condors/runmc_$co_$1_$6_$2_$3_$4_$5.job
 	#cat run_PC$c.job
 	echo '-------------------------------------------------------------------'
-	echo '-------------------------------------------------------------------' >>~/getflow/txts/$1_log.txt
+	echo '-------------------------------------------------------------------' >>~/getflow/txts/$1_$6_log.txt
 
 	#exec 0<&1
 
 	read -p "Do you wish to remove old folder and rerun? (y/n) " answer
 	if [[ $answer =~ ^[Yy]$ ]]; then
 		echo "deleting original run folder"
-		rm -rf /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$co'_MCEff_'$2_$3_$4_$5/
+		rm -rf /atlasgpfs01/usatlas/data/cher97/$1_$6_pnfs_$co'_MCEff_'$2_$3_$4_$5/
 	else
 		echo "writing to old folder"
 	fi
 
-	mkdir -p /atlasgpfs01/usatlas/data/cher97/$1_pnfs_$co'_MCEff_'$2_$3_$4_$5/
-	echo $1_pnfs_$co'_MCEff_'$2_$3_$4_$5
-	echo $1_pnfs_$co'_MCEff_'$2_$3_$4_$5 >>~/getflow/txts/$1_log.txt
+	mkdir -p /atlasgpfs01/usatlas/data/cher97/$1_$6_pnfs_$co'_MCEff_'$2_$3_$4_$5/
+	echo $1_$6_pnfs_$co'_MCEff_'$2_$3_$4_$5
+	echo $1_$6_pnfs_$co'_MCEff_'$2_$3_$4_$5 >>~/getflow/txts/$1_$6_log.txt
 	#cat ~/getflow/condors/runmc_$co_$1_$2_$3_$4_$5.job
-	condor_submit ~/getflow/condors/runmc_$co_$1_$2_$3_$4_$5.job
+	condor_submit ~/getflow/condors/runmc_$co_$1_$6_$2_$3_$4_$5.job
 	linenumber=$((linenumber + 1))
-	echo 'line_-------------------------------------------------------------' >>~/getflow/txts/$1_log.txt
+	echo 'line_-------------------------------------------------------------' >>~/getflow/txts/$1_$6_log.txt
 	echo 'line_'${bold}$linenumber${normal}'-------------------------------------------------------------'
 done 3<$input
